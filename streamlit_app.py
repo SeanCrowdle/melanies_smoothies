c14af0162e8d49315b071499b99d7de046b9569a
SeanCrowdle <seancrowdle@gmail.com>
2025-04-29 15:18:19 +0100

    Update streamlit_app.py
    Added in 3 lines of code at bottom to count responses

--- streamlit_app.py
+++ streamlit_app.py
@@ -23,7 +23,9 @@ ingredients_list = st.multiselect(
     my_dataframe,
     max_selections=5
 )
-
+import requests
+smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+st.text(smoothiefroot_response)
 
 
 if ingredients_list:
